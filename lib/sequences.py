be29057e719e7d939144cd52d6eb8c6d9adaa638
samuel mwangi githaiga <samuelgithaiga913@gmail.com>
2024-05-31 15:34:14 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibonacci_sequence = [0, 1] 
+        for i in range(2, length):
+            next_fibonacci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_fibonacci)
+        print(fibonacci_sequence)
